37d5bdcab38b058f096bcac4bfbe97c1053fa4cb
Akshay Bist <elssar@altrawcode.com>
2023-06-05 18:31:30 +0200

    Add schema for user and user access token
    Added database schemas for the user and their access token tables.

--- db/schemas/jti-schema.sql
+++ db/schemas/jti-schema.sql
@@ -7,3 +7,34 @@ CREATE TABLE IF NOT EXISTS healthcheck (
     created_at timestamp DEFAULT current_timestamp
 );
 
+/*
+    user table
+
+    Primary key is an auto increment integer id, and the email has a unique index on it
+*/
+CREATE TABLE IF NOT EXISTS "user" (
+    id bigserial PRIMARY KEY,
+    email varchar(128) NOT NULL,
+    name varchar(128) NOT NULL,
+    active boolean NOT NULL DEFAULT true,
+    joined timestamp NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp NOT NULL DEFAULT current_timestamp
+);
+
+CREATE UNIQUE INDEX user_email ON "user" (email);
+
+/*
+    token table
+*/
+CREATE TABLE IF NOT EXISTS token (
+    id bigserial PRIMARY KEY,
+    "user" bigint NOT NULL,
+    "public" varchar(8) NOT NULL,
+    "private" varchar(40) NOT NULL,
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp NOT NULL DEFAULT current_timestamp
+);
+
+CREATE UNIQUE INDEX token_user on token ("user");
+CREATE UNIQUE INDEX token_pub on token ("public");
+
